10ca11217d51757ceb9188efbbcc9ee76d229de4
IMKevinJiang <1139962358@qq.com>
2023-12-19 16:17:05 +0800

    jkw

--- train.py
+++ train.py
@@ -3,13 +3,15 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Load a model
-    model = YOLO("model\yolov8m.pt")
+    model = YOLO("yolov8m.yaml")
+    model.info()  # display model information
 
     # Train the model
     results = model.train(
-        data="scripts\housenumber.yaml",
+        data="housenumber.yaml",
         epochs=100,
         imgsz=640,
         workers=4,
         save_period=10,
+        amp=False,
     )
