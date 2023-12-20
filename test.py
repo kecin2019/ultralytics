72c9fe51215b877169fb7c91a36d9696b83ff04a
IMKevinJiang <1139962358@qq.com>
2023-12-20 11:38:22 +0800

    jkw

--- test.py
+++ test.py
@@ -7,3 +7,9 @@ if __name__ == "__main__":
     source = "datasets\\housenumber\\images\\test"
     # 对来源进行推理
     results = model(source, stream=True)  # Results 对象的生成器
+    # 处理结果生成器
+    for result in results:
+        boxes = result.boxes  # 边界框输出的 Boxes 对象
+        masks = result.masks  # 分割掩码输出的 Masks 对象
+        keypoints = result.keypoints  # 姿态输出的 Keypoints 对象
+        probs = result.probs  # 分类输出的 Probs 对象
